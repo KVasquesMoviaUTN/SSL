dadb29bc5a228da56f2e3c8d1b6f3ff0c576f107
Kalil Vasques Movia <kvasquesmovia@frba.utn.edu.ar>
2023-05-30 10:27:50 -0300

    Update hello.C

--- 00-CHelloWorld/hello.C
+++ 00-CHelloWorld/hello.C
@@ -1,10 +1,5 @@
 #include <stdio.h>
 int main() {
-	char cadena[] = "hola mundo";
-	FILE *f;
   printf("Hello, World!");
-	f = fopen("output.txt", "w");
-	fwrite(cadena, 1, sizeof(cadena), f);
-	fclose(f);
   return 0;
 }
